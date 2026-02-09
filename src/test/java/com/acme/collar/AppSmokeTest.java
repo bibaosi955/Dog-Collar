43f47c6316fb4602a30427942cf9e25cdef4a9ee
shiguodong <guodong.shi@petkit.com>
2026-02-09 16:20:29 +0800

    chore: bootstrap spring boot monolith

---
+++ src/test/java/com/acme/collar/AppSmokeTest.java
@@ -0,0 +1,13 @@
+package com.acme.collar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSmokeTest {
+
+  @Test
+  void contextLoads() {
+    // 只要 Spring 容器能启动就算通过
+  }
+}
